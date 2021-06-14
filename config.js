665c204c6d84e6e1b5441ced3604386c38ff22fe
Quanh2512 <85884507+Quanh2512@users.noreply.github.com>
2021-06-14 22:45:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'An Chi à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trái đất vốn lạ thường 🌏
+    Mà sao em cứ đi nhầm đường 👀👣
+    Lạc vào tim anh lẻ loi 💓
+    Đằng sau chữ yêu đây là thương💓😘`,
     btnIntro: 'keke',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😍',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy🙆 ',
     btnYes: 'Siêu siêu siêu iêu anh 💕',
     btnNo: 'Em siêu siêu ghét anh 😭',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh 😍',
     btnReply: 'Gửi cho anh 😘',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu 💞💞💞💞',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Về Hà Nội anh đưa em đi nhé 💑 .',
+    btnAccept: 'Okiiiii lun 😍😘',
+    messLink: 'https://www.facebook.com/messages/t/100054665094557/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
