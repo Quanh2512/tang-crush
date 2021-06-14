f873c58fa6cb09d0e127070537e83240d6c65c29
Quanh2512 <85884507+Quanh2512@users.noreply.github.com>
2021-06-14 23:42:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Về Hà Nội anh đưa em đi nhé 💑 .',
     btnAccept: 'Okiiiii lun 😍😘',
-    messLink: 'file:///C:/Users/Minh%20Pe/Pictures/GIF/An%20Chi.gif' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/anh.quanh.353250' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
